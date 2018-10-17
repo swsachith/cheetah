73245cfd2017b8db96d3335511fb16b7d540503f
Kshitij Mehta <kshitij-v-mehta@users.noreply.github.com>
2018-10-17 12:52:10 -0400

    ADIOS2 interface: basic implementation for engine, transport, and variable operations.

--- codar/cheetah/adios2_interface.py
+++ codar/cheetah/adios2_interface.py
@@ -29,7 +29,16 @@ def set_engine(xmlfile, io_obj, engine_type, parameters=None):
     :return: True on success, False on error
     """
 
-    _set_element(xmlfile, io_obj, "engine", engine_type, None, parameters)
+    tree = ET.parse(xmlfile)
+    io_node = _get_io_node(tree, io_obj)
+    node = ET.Element("engine")
+    node.set('type', engine_type)
+    _add_parameters(node, parameters)
+
+    _replace_and_add_elem(io_node, node, "engine")
+
+    # Write the file back
+    tree.write(xmlfile, xml_declaration=True)
 
 
 def set_transport(xmlfile, io_obj, transport_type, parameters=None):
@@ -43,87 +52,81 @@ def set_transport(xmlfile, io_obj, transport_type, parameters=None):
     :return: True on success, False on error
     """
 
-    _set_element(xmlfile, io_obj, "transport", transport_type, None, 
-                 parameters)
+    tree = ET.parse(xmlfile)
+    io_node = _get_io_node(tree, io_obj)
+    node = ET.Element("transport")
+    node.set('type', transport_type)
+    _add_parameters(node, parameters)
+
+    _replace_and_add_elem(io_node, node, "transport")
+
+    # Write the file back
+    tree.write(xmlfile, xml_declaration=True)
 
 
-def set_var_operation(xmlfile, io_obj, var, op_type, parameters=None):
+def set_var_operation(xmlfile, io_obj, var_name, operation, parameters=None):
     """
     Set an operation on a variable
 
     :param xmlfile: String. The ADIOS2 xml file to be modified
     :param io_obj: String. Name of the io object that contains the engine
-    :param var String. Name of the variable
+    :param var_name String. Name of the variable
+    :param operation String. The operation to be performed on the variable
     :param parameters: A dict containing the parameter keys and values
     :return: True on success, False on error
     """
 
-    _set_element(xmlfile, io_obj, "variable", "var", op_type, parameters)
-
-
-def _set_element(xmlfile, io_obj, io_child_tag, tag_value,
-                 op_type, parameters=None):
-    """
-
-    :param xmlfile: The ADIOS2 xml file to be modified
-    :param io_obj: Name of the io object
-    :param io_child_tag: engine/transport/variable
-    :param tag_value: value to be inserted
-    :param op_type: operation value (compression type) for variable
-    compression
-    :param parameters: dict of parameters with a key and value
-    :return:
-    """
-
-    # Ensure parameters is a list of dicts
     tree = ET.parse(xmlfile)
-    root = tree.getroot()
-    io_found = False
-    for child in root:
-        if child.attrib['name'] == io_obj:
-            io_found = True
-            break
+    io_node = _get_io_node(tree, io_obj)
+    oper_child = ET.Element("operation")
+    oper_child.set('type', operation)
+    _add_parameters(oper_child, parameters)
 
-    if not io_found:
-        raise Exception("Could not find io object matching {0}".format(io_obj))
+    # Check if a 'variable' element exists. If not, create one
+    var_nodes = io_node.findall("variable")
+    for varnode in var_nodes:
+        if varnode.attrib['name'] == var_name:
+            _replace_and_add_elem(varnode, oper_child, "operation")
+            return
 
-    # Whats the keyword that follows the tag
-    if io_child_tag == "engine" or io_child_tag == 'transport':
-        tag_key = 'type'
-    else:  # if io_child_tag == 'variable'
-        tag_key = 'name'
-
-    # Create new node for the new child tag
-    new_node = ET.Element(io_child_tag)
-    new_node.set(tag_key, tag_value)
-
-    if io_child_tag == 'variable':
-        optype_node = ET.Element("operation")
-        optype_node.set('type', op_type)
-        new_node.append(optype_node)
-        new_node = optype_node
-
-    # Add parameters
-    if parameters is not None:
-        for key, value in parameters.items():
-            par_elem = ET.Element("parameter")
-            par_elem.set(key, str(value))
-            new_node.append(par_elem)
-
-    # Remove old node
-    existing_node = child.find(io_child_tag)
-    if existing_node is not None: child.remove(existing_node)
-    child.append(new_node)
+    # Not found. Create new
+    new_var_node = ET.Element("variable")
+    new_var_node.set("name", var_name)
+    new_var_node.append(oper_child)
+    io_node.append(new_var_node)
 
     # Write the file back
     tree.write(xmlfile, xml_declaration=True)
 
-    # All done
-    return True
+
+def _get_io_node(tree, io_obj):
+
+    root = tree.getroot()
+    for sub_element in root:
+        if sub_element.attrib['name'] == io_obj:
+            return sub_element
+
+    raise Exception("Could not find io object matching {0}".format(io_obj))
+
+
+def _add_parameters(node, parameters):
+    if parameters is None:
+        return
+
+    for key, value in parameters.items():
+        par_elem = ET.Element("parameter")
+        par_elem.set(key, str(value))
+        node.append(par_elem)
+
+
+def _replace_and_add_elem(parent, child, elem_tag):
+    existing_node = parent.find(elem_tag)
+    if existing_node is not None: parent.remove(existing_node)
+    parent.append(child)
 
 
 if __name__=="__main__":
     set_engine("test.xml", "writer", "SST", {"conn":"flexp",
                                             'kutta': 22})
     set_transport("test.xml", "writer", "File2", {'Library':'MPI', 'ProfileUnits':'Seconds'})
-    set_var_operation("test.xml", "writer", "T", "zfp", {'rate':18})
+    set_var_operation("test.xml", "writer", "T", "zfp", {'rate':180})
